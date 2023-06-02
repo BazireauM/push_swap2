582e48b7c4e95abc7899ef776c78c70ee683406d
Mathias Bazireau <mbazirea@k1r4p2.42mulhouse.fr>
2023-06-02 05:19:11 +0200

    save

--- src/parssing.c
+++ src/parssing.c
@@ -6,7 +6,7 @@
 /*   By: mbazirea <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/02 03:46:20 by mbazirea          #+#    #+#             */
-/*   Updated: 2023/06/02 04:46:22 by mbazirea         ###   ########.fr       */
+/*   Updated: 2023/06/02 05:18:49 by mbazirea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,16 @@
 
 void	parssing(int argc, char *argv[])
 {
-	char *big_str;
+	char	*big_str;
 /*
 	if (argc < 2)
 		return (NULL);
 */
 	big_str = big_strjoin(argc, argv);
-
+	if (test_input_valid(big_str) == 1)
+		write(1, "error\n", 6);
+	else
+		write(1, "OK\n", 3);
 	printf("%s\n", big_str);
 	free(big_str);
 }
--- src/parssing_utils.c
+++ src/parssing_utils.c
@@ -6,7 +6,7 @@
 /*   By: mbazirea <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/02 03:48:56 by mbazirea          #+#    #+#             */
-/*   Updated: 2023/06/02 04:50:07 by mbazirea         ###   ########.fr       */
+/*   Updated: 2023/06/02 05:18:46 by mbazirea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,39 @@
 
 int	test_input_valid(char *str)
 {
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (!ft_isdigit(str[i]) && str[i] != '-' && str[i] != ' ')
+		{
+			printf("1\n");
+			return (1);
+		}
+		i++;
+	}
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] == '-')
+		{
+			if (str[i - 1] != ' ' || !ft_isdigit(str[i + 1]))
+			{
+				printf("2\n");
+				return (1);
+			}
+		}
+		i++;
+	}
 	return (0);
-	str[0] = '\0';
 }
 
-char *big_strjoin(int argc, char *argv[])
+char	*big_strjoin(int argc, char *argv[])
 {
-	char *big_str;
-	char *big_str2;
-	int i;
+	char	*big_str;
+	char	*big_str2;
+	int		i;
 
 	i = 1;
 	big_str = malloc(sizeof(char) * 2);
